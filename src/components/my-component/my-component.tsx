f7654ff61ae41dd4af42df9491a396ed4322a87a
John Jenkins <John.Jenkins@nanoporetech.com>
2020-11-26 22:45:44 +0000

    init

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,38 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  scoped: true
+})
+export class MyComponent {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <div style={{background: 'black'}}>
+      Hello, World! I'm {this.getText()}
+        <div style={{background: 'red'}}>
+          Here is my slot. It is red.
+          <slot></slot>
+        </div>
+      </div>;
+  }
+}
